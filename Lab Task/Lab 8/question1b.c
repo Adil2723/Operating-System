7899a3fdf05c06d521ea741f614b3e3cf9eef24b
Muhammad Adil Saeed <masadil942@gmail.com>
2026-05-12 11:57:00 +0500

    Implement multithreading with mutex for shared variable
    This program creates two threads that increment a shared variable using a mutex for synchronization.

---
+++ Lab Task/Lab 8/question1b.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int shared = 0;
+pthread_mutex_t lock;
+
+void* increment(void* arg) {
+    for(int i = 0; i < 5; i++) {
+        pthread_mutex_lock(&lock);
+
+        shared++;
+
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Final value (with mutex): %d\n", shared);
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
