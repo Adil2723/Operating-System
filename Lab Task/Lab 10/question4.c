2ad39f47ecff2a85933913eed011b1dc8879201a
Muhammad Adil Saeed <masadil942@gmail.com>
2026-05-17 22:48:20 +0500

    Implement signal handling for SIGINT and SIGCHLD

--- Lab Task/Lab 10/question4.c
+++ Lab Task/Lab 10/question4.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h> 
+#include <signal.h> 
+#include <unistd.h> 
+#include <sys/wait.h> 
+ 
+void handle_sigint(int sig) { 
+    printf("\nCtrl+C pressed! Not exiting...\n"); 
+} 
+ 
+void handle_sigchld(int sig) { 
+    printf("Child process terminated!\n"); 
+} 
+ 
+int main() { 
+    signal(SIGINT, handle_sigint); 
+    signal(SIGCHLD, handle_sigchld); 
+ 
+    pid_t pid = fork(); 
+ 
+    if (pid == 0) { 
+        printf("Child running...\n"); 
+        sleep(2); 
+        printf("Child exiting...\n"); 
+    } else { 
+        while (1) { 
+            printf("Parent running...\n"); 
+            sleep(1); 
+        } 
+    } 
+ 
+    return 0; 
+}
